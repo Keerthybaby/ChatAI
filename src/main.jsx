4a8b81169e072fd1d91d1a0f56125ea762e78b78
Keerthy Baby <keerthybaby025@gmail.com>
2025-05-28 10:41:28 +0530

    create react layout with outlet

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Homepage from "./routes/homepage/Homepage";
 import DashboardPage from "./routes/dashboardPage/DashboardPage";
 import ChatPage from "./routes/chatPage/ChatPage";
+import RootLayout from "./layouts/rootLayout/RootLayout";
+import DashboardLayout from "./layouts/dashboardLayout/DashboardLayout";
+
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Homepage />,
-  },
-  {
-    path: "/dashboard",
-    children: [
-      { path: "/dashboard", element: <DashboardPage /> },
-      { path: "/dashboard/chat/:id", element: <ChatPage /> },
-    ],
-  },
+    element:<RootLayout/>,
+    children:[
+      {
+        path:"/",
+        element:<Homepage/>,
+      },
+      {
+        element:<DashboardLayout/>,
+        children:[
+          {
+            path:"/dashboard",
+            element:<DashboardPage/>
+          },
+          {
+            path:"/dashboard/chats/:id",
+            element:<ChatPage/>
+          }
+        ]
+      }
+    ]
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
